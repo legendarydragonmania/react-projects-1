cf8ab61507936070c02d7f6030454826a9a76c39
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-04-14 21:38:10 +0500

    master

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,7 +4,7 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [jobs, setJobs] = useState([])
   const [value, setValue] = useState(0)
 
@@ -13,8 +13,8 @@ function App() {
       const response = await fetch(url)
       if (response.status >= 200 && response.status <= 299) {
         const data = await response.json()
-        console.log(data)
-        return data
+        setJobs(data)
+        setLoading(false)
       } else {
         throw new Error(`There was an error with the servers!`)
       }
@@ -27,7 +27,53 @@ function App() {
     fetchJobs()
   }, [])
 
-  return
+  if (loading) {
+    return (
+      <section className='loading section'>
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+
+  const { company, dates, duties, title } = jobs[value]
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* btn-container */}
+        <div className='btn-container'>
+          {jobs.map((job, index) => {
+            return (
+              <button
+                key={job.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {job.company}
+              </button>
+            )
+          })}
+        </div>
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div className='job-desc' key={index}>
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default App
