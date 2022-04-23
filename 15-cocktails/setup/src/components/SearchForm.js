408de19c1fa754a023b0694410b5e80baa87ab51
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-04-23 18:05:21 +0500

    master

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext()
+  const searchValue = useRef('')
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+
+  React.useEffect(() => {
+    searchValue.current.focus()
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='search seaction'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorit cocktail</label>
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo cum inventore dicta molestiae. Consectetur reiciendis, exercitationem repudiandae quas sed asperiores?</p>
+    </section>
   )
 }
 
