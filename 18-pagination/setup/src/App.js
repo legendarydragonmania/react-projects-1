38ca722391089143b34ca689b24eb648aee878f9
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-04-26 18:37:57 +0500

    master

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -2,7 +2,49 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch()
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState([])
+
+  useEffect(() => {
+    if (loading) return
+    setFollowers(data[page])
+  }, [loading, page])
+
+  const handlePage = (index) => {
+    setPage(index)
+  }
+
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>{loading ? 'Loading...' : 'Pagination'}</h1>
+        <div className='underline'></div>
+      </div>
+      <section className='followers'>
+        <div className='container'>
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />
+          })}
+        </div>
+        {!loading && (
+          <div className='btn-container'>
+            {data.map((item, index) => {
+              return (
+                <button
+                  className='page-btn'
+                  key={index}
+                  onClick={handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              )
+            })}
+          </div>
+        )}
+      </section>
+    </main>
+  )
 }
 
 export default App
--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,13 @@
-const paginate = () => {}
+const paginate = (follower) => {
+  const itemsPerPage = 10
+  const numberOfPages = Math.ceil(follower.length / itemsPerPage)
+
+  const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+    const start = index * itemsPerPage
+    return follower.slice(start, start + itemsPerPage)
+  })
+
+  return newFollowers
+}
 
 export default paginate
